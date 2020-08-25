2fa48cfeb8deb49be423b6106e6a3870516609c0
Jaime Alejandro Perez Frausto <jaime.a.p.frausto@gmail.com>
2020-08-24 17:37:30 -0700

    code fixes

--- Python Functions, Files, and Dictionaries/project.py
+++ Python Functions, Files, and Dictionaries/project.py
@@ -1,7 +1,7 @@
 # definitions
 # Creating a strip punctuation function 
 def strip_punctuation(string):
-# iterating through the string
+# loop to iterate through the string
     for item in punctuation_chars:
 # replace punctuation with a space if its contained in string
         string = string.replace(item, '')
@@ -11,15 +11,15 @@ def strip_punctuation(string):
 def get_pos(str_sentences):
 # making sentences lower case
     str_sentences = str_sentences.lower()
-# run line through the strip definition
+# run each line through the strip definition
     lines = strip_punctuation(str_sentences)
 # counting variable
-    count_positive = 0
-# iterating through words and increasing positivie count if words in positive_words is true
+    count_pos = 0
+# looping lines
     for words in lines.split():
         if words in positive_words:
-            count_positive += 1
-    return count_positive
+            count_pos += 1
+    return count_pos
 
 def get_neg(str_sentences):
 # making sentences lower case
@@ -28,13 +28,13 @@ def get_neg(str_sentences):
     lines = strip_punctuation(str_sentences)
 # counting variable
     count_negative = 0
-# Iterate through words and increasing negative count if words in negative_words is true
+# looping lines
     for words in lines.split():
         if words in negative_words:
             count_negative += 1
     return count_negative
-#---------------------------------------------------------------------------
-# Begin  project code
+# ----------------------------------------------------------------------
+# Begin code
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 # lists of words to use
 positive_words = []
@@ -49,7 +49,7 @@ with open("negative_words.txt") as pos_f:
     for lin in pos_f:
         if lin[0] != ';' and lin[0] != '\n':
             negative_words.append(lin.strip())
-#---------------------------------------------------------------------------
+#----------------------------------------------------
 # lists for tweet texts, replies, retweets and scores
 tweet_text = []
 tweet_replies = []
@@ -63,33 +63,29 @@ with open("project_twitter_data.csv") as twitter_data:
     for line in lines:
 # splitting tweets line by line
         words = line.split(",")
-# making all tweets into tuples and appending them to tweet_texts
+# making all tweets into tupples and appending them to tweet_texts
         for tweets in words[0:1]:
-            word = tweets.split()
-            aTuple = tuple(word)
-            tweet_text.append(aTuple)
+            tweet_text.append(tweets)
 # appending the number of retweets to tweet_retweets
-        tweet_retweets.append(words[1].strip())
+            tweet_retweets.append(words[1].strip())
 # appending then number of replies to tweet_replies
-        tweet_replies.append(words[2].strip())
-#---------------------------------------------------------------------------
+            tweet_replies.append(words[2].strip())
+#---------------------------------------------------------------
 # calculating positive scores and negative scores for each tweet
-for items in tweet_text:
-# counting variables
-    positive = 0
-    negative = 0
-# iterating items and counting each positive and negative words.Counts are appended to positive and negative score lists
-    for words in items:
-        if words in positive_words:
-            positive += 1
-        if words in negative_words:
-            negative +=1
+for tweets in tweet_text:
+# setting positive as the number of positive words in a tweet. Returned value is appended to positive_scores
+    positive = get_pos(tweets)
     positive_scores.append(positive)
+# setting negative as the number of negative words in a tweet. Returned value is appended to negative_scores
+    negative = get_neg(tweets)
     negative_scores.append(negative)
 # calculating the net scores from positive_scores and negative_scores lists
 for a,b in zip(positive_scores,negative_scores):
     net_scores.append(str(a-b))
 #---------------------------------------------------------------------------
+print(positive_scores)
+print(negative_scores)
+print(net_scores)
 # writing to resulting_data.csv file
 with open("resulting_data.csv","w") as result_data:
 # writing headers and data
